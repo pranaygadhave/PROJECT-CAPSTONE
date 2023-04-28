7ab9037f6fd74409885c43c88acb687d86ad6090
pranaygadhave <71037460+pranaygadhave@users.noreply.github.com>
2023-04-28 16:47:12 +0530

    Delete capstone/employee-management-system directory

--- capstone/employee-management-system/src/main/java/com/capstone/project/exception/EmployeeNotFound.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.project.exception;
-
-public class EmployeeNotFound extends Exception {
-	public EmployeeNotFound(String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-	
-}
